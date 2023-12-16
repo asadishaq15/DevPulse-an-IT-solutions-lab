26dea05590d013f4514ca415e332a5c939498371
asadishaq15 <asadishaq392@gmail.com>
2023-12-17 01:47:32 +0500

    added router in app.js

--- devPulse-frontend/frontend/src/App.js
+++ devPulse-frontend/frontend/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+// Import necessary libraries and components
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Import your components
+
+// Import your CSS file
 import './App.css';
+import Home from './components/home';
+import About from './components/about';
+import Services from './components/services';
+import Team from './components/team';
+import Careers from './components/careers';
+import Contact from './components/contact';
+import Faq from './components/faq';
+import Portfolio from './components/portfolio';
+import Navbar from './components/navbar';
+import Footer from './components/foot';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        {/* For example: <Header /> */}
+
+        <Routes>
+          <Route path="/" exact component={<Home/>} />
+          <Route path="/about" component={<About/>} />
+          <Route path="/services" component={<Services/>} />
+          <Route path="/portfolio" component={<Portfolio/>} />
+          <Route path="/team" component={<Team/>} />
+          <Route path="/careers" component={<Careers/>} />
+          <Route path="/contact" component={<Contact/>} />
+          <Route path="/faq" component={<Faq/>} />
+        </Routes>
+
+        {/* Include your footer component here */}
+      <Footer/>
+      </div>
+    </Router>
   );
 }
 
